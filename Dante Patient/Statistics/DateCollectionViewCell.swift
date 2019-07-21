af79561ab154b91b9ace032601df6170190d2df6
Xinhao Liang <xinhao128@hotmail.com>
2019-07-21 02:36:48 -0700

    App Store Icon image not transparent

---
+++ Dante Patient/Statistics/DateCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DateCollectionViewCell.swift
+//  Dante Patient
+//
+//  Created by Xinhao Liang on 7/21/19.
+//  Copyright © 2019 Xinhao Liang. All rights reserved.
+//
+
+import UIKit
+
+class DateCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Dante Patient/Statistics/VisitsHistoryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  VisitsHistoryViewController.swift
+//  Dante Patient
+//
+//  Created by Xinhao Liang on 7/21/19.
+//  Copyright © 2019 Xinhao Liang. All rights reserved.
+//
+
+import UIKit
+
+class VisitsHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
