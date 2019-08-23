d172e3aace387b65d8f0ad9684c71e90b1adbf52
Xinhao Liang <xinhao128@hotmail.com>
2019-08-23 11:43:43 -0700

    added WR

--- Dante Patient/Oncology Map/OncMapViewController.swift
+++ Dante Patient/Oncology Map/OncMapViewController.swift
@@ -27,7 +27,8 @@ class OncMapViewController: UIViewController, UIScrollViewDelegate, FloatingPane
     var mapDict: [String: [(Double, Double)]] = [
         "LA1": [(0.38, 0.7), (0.41, 0.75), (0.46, 0.75), (0.48, 0.7), (0.39, 0.8)],
         "TLA": [(0.9, 0.36), (0.95, 0.5), (0.83, 0.54), (0.8, 0.5), (0.86, 0.4)],
-        "CT": [(0.11, 0.7), (0.03, 0.75), (0.12, 0.75), (0.06, 0.7), (0.04, 0.8)]
+        "CT": [(0.11, 0.7), (0.03, 0.75), (0.12, 0.75), (0.06, 0.7), (0.04, 0.8)],
+        "WR": [(0.27, 0.41), (0.3, 0.41), (0.27, 0.47), (0.31, 0.47), (0.33, 0.45)]
     ]
     
     override func viewDidLoad() {
@@ -104,7 +105,7 @@ class OncMapViewController: UIViewController, UIScrollViewDelegate, FloatingPane
         
         //  Add FloatingPanel to a view with animation.
         fpc.addPanel(toParent: self, animated: true)
-        
+
         // call observe to always listen for event changes
         ref.child("StaffLocation").observe(.value, with: {(snapshot) in
             var mapDictCopy = self.mapDict
