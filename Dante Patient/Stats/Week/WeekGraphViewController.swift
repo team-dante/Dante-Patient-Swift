e04fc550612d152123edae236382aed9bdddb8df
Xinhao Liang <xinhao128@hotmail.com>
2019-09-08 15:39:24 -0700

    Added tableView sections to group patient visit dates by week

---
+++ Dante Patient/Stats/Week/WeekGraphViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeekGraphViewController.swift
+//  Dante Patient
+//
+//  Created by Xinhao Liang on 9/7/19.
+//  Copyright © 2019 Xinhao Liang. All rights reserved.
+//
+
+import UIKit
+
+class WeekGraphViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Dante Patient/Utilities/DateExtension.swift
@@ -0,0 +1,9 @@
+//
+//  DateExtension.swift
+//  Dante Patient
+//
+//  Created by Xinhao Liang on 9/6/19.
+//  Copyright © 2019 Xinhao Liang. All rights reserved.
+//
+
+import Foundation
