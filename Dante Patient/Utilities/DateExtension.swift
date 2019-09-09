2dd55881bc83f36979b929afeeb6ca3ccd80d424
Xinhao Liang <xinhao128@hotmail.com>
2019-09-08 17:27:50 -0700

    Fix week range

--- Dante Patient/Utilities/DateExtension.swift
+++ Dante Patient/Utilities/DateExtension.swift
@@ -12,12 +12,12 @@ extension Date {
     var startOfWeek: Date? {
         let gregorian = Calendar(identifier: .gregorian)
         guard let sunday = gregorian.date(from: gregorian.dateComponents([.yearForWeekOfYear, .weekOfYear], from: self)) else { return nil }
-        return gregorian.date(byAdding: .day, value: 1, to: sunday)
+        return gregorian.date(byAdding: .day, value: 0, to: sunday)
     }
     
     var endOfWeek: Date? {
         let gregorian = Calendar(identifier: .gregorian)
         guard let sunday = gregorian.date(from: gregorian.dateComponents([.yearForWeekOfYear, .weekOfYear], from: self)) else { return nil }
-        return gregorian.date(byAdding: .day, value: 7, to: sunday)
+        return gregorian.date(byAdding: .day, value: 6, to: sunday)
     }
 }
